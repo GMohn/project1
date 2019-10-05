23364ddb12576c95e99f7af02118a790830992d0
Oracity <gmohn415@gmail.com>
2019-10-05 02:33:28 -0700

    Update StringUtils.cpp

--- proj1/src/StringUtils.cpp
+++ proj1/src/StringUtils.cpp
@@ -7,58 +7,72 @@ namespace StringUtils{
     
 std::string Slice(const std::string &str, ssize_t start, ssize_t end){
     // Your code goes here
+    return "";
 }
 
 std::string Capitalize(const std::string &str){
     // Your code goes here
+    return "";
 }
 
 std::string Title(const std::string &str){
     // Your code goes here
+    return "";
 }
 
 std::string LStrip(const std::string &str){
     // Your code goes here
+    return "";
 }
 
 std::string RStrip(const std::string &str){
     // Your code goes here
+    return "";
 }
 
 std::string Strip(const std::string &str){
     // Your code goes here
+    return "";
 }
 
 std::string Center(const std::string &str, int width, char fill){
     // Your code goes here
+    return "";
 }
 
 std::string LJust(const std::string &str, int width, char fill){
     // Your code goes here
+    return "";
 }
 
 std::string RJust(const std::string &str, int width, char fill){
     // Your code goes here
+    return "";
 }
 
 std::string Replace(const std::string &str, const std::string &old, const std::string &rep){
     // Your code goes here
+    return "";
 }
 
 std::vector< std::string > Split(const std::string &str, const std::string &splt){
     // Your code goes here
+    return {};
 }
 
 std::string Join(const std::string &str, const std::vector< std::string > &vect){
-    // Your code goes here
+    // Your code goes 
+    return "";
 }
 
 std::string ExpandTabs(const std::string &str, int tabsize){
     // Your code goes here
+    return "";
 }
 
 int EditDistance(const std::string &left, const std::string &right, bool ignorecase){
     // Your code goes here
+    return 0;
 }
 
 }
